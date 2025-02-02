2bffcbab4c18b7fd6300d9ceb6c238b5bbfa42ef
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-02-02 22:17:35 +0500

    get and set video properties

--- src/App.js
+++ src/App.js
@@ -12,6 +12,25 @@ const App = () => {
     const net = await facemash.load(facemash.SupportedPackage.mediapipeFacemesh)
   }
 
+  const detect = async(net) => {
+    if(typeof webcamRef.current !== 'undefined' && 
+      webcamRef.current !== null && 
+      webcamRef.current.video.readyState === 4 
+    ){
+
+      const video = webcamRef.current.video
+      const videoWidth = webcamRef.current.video.videoWidth
+      const videoHeigth = webcamRef.current.video.videoHeigth
+
+
+      webcamRef.current.video.width = videoWidth
+      webcamRef.current.video.heigth =videoHeigth
+
+      canvasRef.current.width = videoWidth
+      canvasRef.current.heigth = videoHeigth
+    }
+  }
+
   return (
     <div className='App'>
       <header className='App-header'>

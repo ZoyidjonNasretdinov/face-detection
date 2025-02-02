2bb120f11753b012fb492c93e96fac3605459882
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-02-02 21:46:10 +0500

    Import Webcam and model

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {useRef, useEffect} from 'react'
+import './App.css'
+import * as facemash from '@tensorflow-models/face-landmarks-detection'
+import Webcam from 'react-webcam'
+
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App

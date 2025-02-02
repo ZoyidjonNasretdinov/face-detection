1479e6f97635ac5cb036c6ce3910db29b57e61ed
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-02-02 21:58:13 +0500

    Add Canvas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Webcam from 'react-webcam'
 
 const App = () => {
   const webcamRef = useRef(null)
+  const canvasRef = useRef(null)
 
 
 
@@ -13,6 +14,7 @@ const App = () => {
     <div className='App'>
       <header className='App-header'>
         <Webcam ref={webcamRef} className='webcam'/>
+        <canvas ref={canvasRef} className='canvas'/>
       </header>
     </div>
   )
